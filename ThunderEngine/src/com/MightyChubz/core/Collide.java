fafc11ac2fdf9920abe5007ed6af3e5993ed7be9
MightyChubz <alexc@occultvisions.com>
2016-02-18 07:40:02 -0500

    Added collision class. Has no use at the moment.

---
+++ ThunderEngine/src/com/MightyChubz/core/Collide.java
@@ -0,0 +1,6 @@
+package com.MightyChubz.core;
+
+// Has no use at the moment, don't use.
+public class Collide {
+
+}
--- ThunderEngine/src/com/MightyChubz/core/GameObject.java
+++ ThunderEngine/src/com/MightyChubz/core/GameObject.java
@@ -5,4 +5,5 @@ import com.MightyChubz.core.mathf.Transform;
 public class GameObject {
     public SpriteRenderer renderer = new SpriteRenderer();
     public Transform transform = new Transform();
+    public Collide collider = new Collide();
 }
