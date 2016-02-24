d1140a128d562b9e9cfa637ed8f4d74b816e7936
MightyChubz <alexc@occultvisions.com>
2016-02-24 17:54:32 -0500

    Removed todo comment.

--- ThunderEngine/src/com/MightyChubz/core/CollisionBox.java
+++ ThunderEngine/src/com/MightyChubz/core/CollisionBox.java
@@ -3,7 +3,6 @@ package com.MightyChubz.core;
 import com.MightyChubz.core.mathf.Size;
 import com.MightyChubz.core.mathf.Vector2f;
 
-// TODO: Fully incorporate the collision box.
 public class CollisionBox {
     public Vector2f position = new Vector2f(1, 1);
     public Vector2f alignMode = new Vector2f(1, 1);
